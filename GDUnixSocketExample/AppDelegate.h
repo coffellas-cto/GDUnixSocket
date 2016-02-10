b62f2bde91aa1661af027f09a5a2f170bf2c7896
Alex Gordiyenko <cto@coffellas.com>
2016-02-10 20:31:23 +0200

    Initial Commit

---
+++ GDUnixSocketExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDUnixSocketExample
+//
+//  Created by Alex G on 10.02.16.
+//  Copyright © 2016 Alexey Gordiyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDUnixSocketExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDUnixSocketExample
+//
+//  Created by Alex G on 10.02.16.
+//  Copyright © 2016 Alexey Gordiyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
